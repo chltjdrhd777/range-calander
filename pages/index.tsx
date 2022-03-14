a64ab74f11a818400fca51accb26b38772567848
starhn87 <starhn87@gmail.com>
2022-03-14 15:27:10 +0900

    Feat: style jsx 초기 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import css from "styled-jsx/css";
+
+const style = css`
+  * {
+    background-color: red;
+  }
+`;
 
 const Home: NextPage = () => {
-  return <div>케어닥</div>;
+  return (
+    <>
+      <div>케어닥</div>
+      <style jsx>{style}</style>
+    </>
+  );
 };
 
 export default Home;
