e23016a8a39e4361d211715001f876547f25c182
starhn87 <starhn87@gmail.com>
2022-03-14 15:50:17 +0900

    Feat: 리덕스 초기 상태 정의 & swr 설치

--- pages/redux/slice.ts
+++ pages/redux/slice.ts
@@ -1,9 +1,58 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export interface IState {
+  phoneNumber: string;
+  workType: string;
+  schedule: {
+    startDate: string; // yyyy-MM-dd
+    endDate: string; // yyyy-MM-dd
+    visitTime: string; // hh:mm:ss
+    hour: number | null;
+  };
+  address: {
+    addressDetail: string;
+    jibunAddress: string;
+    liName: string | null;
+    locationCode: string;
+    roadCode: string;
+    myundongName: string;
+    roadAddress: string;
+    sidoName: string;
+    sigunguName: string;
+  };
+}
+
+const initialState: IState = {
+  phoneNumber: "",
+  workType: "",
+  schedule: {
+    startDate: "", // yyyy-MM-dd
+    endDate: "", // yyyy-MM-dd
+    visitTime: "", // hh:mm:ss
+    hour: null,
+  },
+  address: {
+    addressDetail: "",
+    jibunAddress: "",
+    liName: null,
+    locationCode: "",
+    roadCode: "",
+    myundongName: "",
+    roadAddress: "",
+    sidoName: "",
+    sigunguName: "",
+  },
+};
+
 const slice = createSlice({
   name: "reducer",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    workType: (state, action) => {},
+    schedule: (state, action) => {},
+    address: (state, action) => {},
+    phoneNumber: (state, action) => {},
+  },
 });
 
 export default slice.reducer;
